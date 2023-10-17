66893754dbef277f204cfb452c11b5d24491a2cb
Yunus Cem Özay <78884331+cemozay@users.noreply.github.com>
2023-10-17 17:51:18 +0300

    minimize to tray. close and prevent sleep functions

--- app/shutdownTimer.py
+++ app/shutdownTimer.py
@@ -1,37 +1,45 @@
 import os
 import customtkinter as ctk
-from tkinter.messagebox import showerror, showwarning
+from tkinter.messagebox import showerror, showwarning, askokcancel
+import ctypes
+from pystray import MenuItem as item, Icon
+from PIL import Image
 
 # Set appearance and color theme
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 
-# Check shutdown timer status
-check_status = "shutdown -s -t 3600"
-shutdown_status = os.system(check_status)
-
 root = ctk.CTk()
 root.title("Shutdown Timer")
 root.resizable(False, False)
-root.iconbitmap("assets/icon.ico")  # Icon path
+root.iconbitmap("assets/icon.ico") # Icon path
 
 # App sizing and center window
 app_width = 400
-app_height = 300
+app_height = 350
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x_coordinates = (screen_width - app_width) / 2
 y_coordinates = (screen_height - app_height) / 2
 root.geometry(f"{app_width}x{app_height}+{int(x_coordinates)}+{int(y_coordinates)}")
 
+global prevent_sleep
+prevent_sleep = False
+
 # Shutdown button function
 def shutdown_clicked():
+    global prevent_sleep
     if entry_hour.get() or entry_minute.get():
         hour = int(entry_hour.get() or 0)
         minute = int(entry_minute.get() or 0)
         seconds = (hour * 3600) + (minute * 60)
 
-        command = "shutdown -s -t {}".format(seconds)
+        # Prevent sleep
+        if disable_sleep.get():
+            ctypes.windll.kernel32.SetThreadExecutionState(0x80000002)
+            prevent_sleep = True
+
+        command = "shutdown -s -t {}".format(seconds)        
         shutdown_error = os.system(command)
         # Check for any command error
         if shutdown_error:
@@ -39,12 +47,19 @@ def shutdown_clicked():
         else:
             shutdown_button.configure(fg_color="#2AA526", hover_color="#1A8A1F", state="disabled", text="Timer Started")
             cancel_button.configure(fg_color="#DB282C", hover_color="#B71C2B", state="normal", text="Cancel Shutdown")
+
     else:
         showwarning(title="Error", message="Input Required!")
 
 # Cancel the shutdown button function
 def shutdown_cancel_clicked():
-    cancel_error = os.system('shutdown -a')
+    global prevent_sleep
+    # Cancel prevent sleep
+    if prevent_sleep:
+        ctypes.windll.kernel32.SetThreadExecutionState(0x00000000)
+        prevent_sleep = False
+
+    cancel_error = os.system('shutdown -a') 
     # Check for any command error
     if cancel_error:
         showerror(title="Error", message="Command could not be executed!")
@@ -64,24 +79,45 @@ entry_hour.pack(padx=20, pady=10)
 entry_minute = ctk.CTkEntry(root, placeholder_text="Minute")
 entry_minute.pack(padx=20, pady=10)
 
+# Create a Checkbutton to disable sleep
+disable_sleep = ctk.BooleanVar()
+disable_sleep_checkbutton = ctk.CTkCheckBox(root, text="Keep Computer Awake", variable=disable_sleep)
+disable_sleep_checkbutton.pack(padx=20, pady=10)
+
 # Shutdown button
 shutdown_button = ctk.CTkButton(root, text="Start Timer", command=shutdown_clicked)
 shutdown_button.pack(padx=20, pady=10)
 
-# Cancel button for an already started timer
-if shutdown_status:
-    shutdown_button.configure(fg_color="#2AA526", hover_color="#1A8A1F", state="disabled", text="Timer Started")
-    cancel_button = ctk.CTkButton(root, text="Cancel Shutdown", fg_color="#DB282C", hover_color="#B71C2B", command=shutdown_cancel_clicked)
-else:
-    os.system('shutdown -a')
-    cancel_button = ctk.CTkButton(root, text="Cancel Shutdown", fg_color="#6c757d", command=shutdown_cancel_clicked, state="disabled")
+# Cancel button
+cancel_button = ctk.CTkButton(root, text="Cancel Shutdown", fg_color="#DB282C", hover_color="#B71C2B", command=shutdown_cancel_clicked)
 cancel_button.pack(padx=20, pady=10)
 
-# Function to handle closing the application
+# Close app function
 def on_closing():
-    os.system('shutdown -a')  # Cancel shutdown if the app is closed
-    root.destroy()
+    global prevent_sleep
+    if askokcancel("Quit", "Are you sure you want to exit and cancel the shutdown timer?"):
+        if prevent_sleep:
+            ctypes.windll.kernel32.SetThreadExecutionState(0x00000000)
+        os.system('shutdown -a')
+        root.destroy()
 
-root.protocol("WM_DELETE_WINDOW", on_closing)
+# Menu quit function
+def quit_window(icon, item):
+    icon.stop()
+    root.after(0,root.deiconify)
+    root.after(500,on_closing)
+
+def show_window(icon, item):
+    icon.stop()
+    root.after(0,root.deiconify)
+
+def withdraw_window():
+    root.withdraw()
+    image = Image.open("assets/icon.ico")
+    menu = (item('Quit', quit_window), item('Show', show_window))
+    icon = Icon("name", image, "title", menu)
+    icon.run()
+
+root.protocol("WM_DELETE_WINDOW", withdraw_window)
 
 root.mainloop()
